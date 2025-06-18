e2de63350efc4cc6f43be21809b00bdb2e389dec
etdong <eric@donger.ca>
2025-06-18 14:04:32 +0800

    secure: false cookie

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: true,
+        secure: false,
         sameSite: 'none',
         maxAge: 1000 * 60 * 60 * 24
     },
